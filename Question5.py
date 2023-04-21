2392fa83edc7bc94ad3f3adb9e461d59ba95319a
hau_v <hau_vu94@hotmail.com>
2023-04-21 15:01:56 +0200

    starting Q5

---
+++ Question5.py
@@ -0,0 +1,29 @@
+import random
+
+FileName = input("Write in the filename with words: ")
+
+try:
+    answer = open(f"{FileName}.txt", "r")
+
+except IOError:
+    print("file not found, please do not write \".txt\"")
+
+except ValueError as exception:
+    print("Error:", str(exception))
+
+readText = answer.readlines()
+answer.close()
+
+wordList = []
+
+for line in readText:
+    line = line.strip()
+    for word in line.split():
+        wordList.append(word)
+
+word = wordList[random.randrange(0, len(wordList) -1)]
+
+
+print(readText)
+print(wordList)
+print(word)
