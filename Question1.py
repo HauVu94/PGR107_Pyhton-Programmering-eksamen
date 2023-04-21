100a24b3a7a43cd0b408a4096fa8d17d6706d69e
RobinFlaks <robin_flaks@hotmail.com>
2023-04-21 10:44:34 +0200

    now reads files

--- Question1.py
+++ Question1.py
@@ -1,1 +1,13 @@
-print("Hello world!")
+
+originalText = input("write the name of the file you want to censor: ")
+bannedText = input("write the name of the file containing banned words: ")
+newText = input("what should we save the file as: ")
+
+try:
+    originalFile = open(f"{originalText}.txt", "r")
+    bannedFile = open(f"{bannedText}.txt", "r")
+except IOError :
+    print("file not found")
+
+except ValueError as exception:
+    print("Error:", str(exception))
