654c49b7caa6f3e6dff7db144b12bc30b1d2b1a6
hau_v <hau_vu94@hotmail.com>
2023-04-21 11:35:45 +0200

    almost finish

--- Question1.py
+++ Question1.py
@@ -11,3 +11,27 @@ except IOError :
 
 except ValueError as exception:
     print("Error:", str(exception))
+
+
+readOrignalFile = originalFile.readlines()
+originalFile.close()
+readBannedFile = bannedFile.readlines()
+bannedFile.close()
+
+bannedList = []
+
+for line in readBannedFile:
+    line = line.rstrip()
+    for word in line.split():
+        bannedList.append(word)
+
+newword = ""
+
+for line in readOrignalFile:
+    for word in bannedList:
+       line = line.replace(word, len(word) * "*")
+    newword = newword + line
+
+newFile = open(f"{newText}.txt", "w")
+newFile.write(newword)
+
