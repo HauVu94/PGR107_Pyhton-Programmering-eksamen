de1d60b51d529510e752beef763bf632cda691c9
RobinFlaks <robin_flaks@hotmail.com>
2023-04-21 11:44:33 +0200

    refactored Question1.py and finished with it!

--- Question1.py
+++ Question1.py
@@ -1,37 +1,37 @@
 
-originalText = input("write the name of the file you want to censor: ")
-bannedText = input("write the name of the file containing banned words: ")
-newText = input("what should we save the file as: ")
+userFile = input("write the name of the file you want to censor: ")
+userBannedList = input("write the name of the file containing banned words: ")
+newFileName = input("what should we save the file as: ")
 
 try:
-    originalFile = open(f"{originalText}.txt", "r")
-    bannedFile = open(f"{bannedText}.txt", "r")
-except IOError :
+    originalFile = open(f"{userFile}.txt", "r")
+    bannedFile = open(f"{userBannedList}.txt", "r")
+
+except IOError:
     print("file not found")
 
 except ValueError as exception:
     print("Error:", str(exception))
 
-
-readOrignalFile = originalFile.readlines()
+originalText = originalFile.readlines()
 originalFile.close()
-readBannedFile = bannedFile.readlines()
+bannedText = bannedFile.readlines()
 bannedFile.close()
 
 bannedList = []
 
-for line in readBannedFile:
+for line in bannedText:
     line = line.rstrip()
     for word in line.split():
         bannedList.append(word)
 
-newword = ""
+newText = ""
 
-for line in readOrignalFile:
+for line in originalText:
     for word in bannedList:
        line = line.replace(word, len(word) * "*")
-    newword = newword + line
+    newText = newText + line
 
-newFile = open(f"{newText}.txt", "w")
-newFile.write(newword)
+newFile = open(f"{newFileName}.txt", "w")
+newFile.write(newText)
 
