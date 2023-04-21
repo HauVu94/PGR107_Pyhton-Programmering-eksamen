b355070d34d753956fe4b983ec0a4debadd783e9
hau_v <hau_vu94@hotmail.com>
2023-04-21 12:55:12 +0200

    Question4 almost complete

--- Question4.py
+++ Question4.py
@@ -1,27 +1,38 @@
 
+def CheckIfSharedCharacters(wordOne, wordTwo):
+    sharedCharacters = []
+    for char in wordOne:
+        if char in wordTwo:
+            sharedCharacters.append(char)
+    return sharedCharacters
+
+def CheckIfUniqueCharacters(wordOne, wordTwo):
+    uniqueCharacters = []
+    for char in wordOne:
+        if char not in wordTwo:
+            uniqueCharacters.append(char)
+    for char in wordTwo:
+        if char not in wordOne:
+            uniqueCharacters.append(char)
+    return uniqueCharacters
+
+def CheckNonOcurringCharacters(sharedCharacters, uniqueCharacters):
+    missingCharacters = []
+    for char in set("abcdefghijklmnopqrstuvwxyzæøå"):
+        if char not in uniqueCharacters:
+            if char not in sharedCharacters:
+                missingCharacters.append(char)
+
+
 wordOne = input("word one: ")
 wordTwo = input("word two: ")
 
 wordOne = set(wordOne)
 wordTwo = set(wordTwo)
 
-sharedCharacters = []
-uniqueCharacters = []
+sharedCharacters = CheckIfSharedCharacters(wordOne, wordTwo)
+uniqueCharacters = CheckIfUniqueCharacters(wordOne, wordTwo)
 missingCharacters = []
-
-for char in wordOne:
-    if char in wordTwo:
-        sharedCharacters.append(char)
-    else:
-        uniqueCharacters.append(char)
-for char in wordTwo:
-    if char not in sharedCharacters:
-        uniqueCharacters.append(char)
-
-for char in set("abcdefghijklmnopqrstuvwxyzæøå"):
-    if char not in uniqueCharacters:
-        if char not in sharedCharacters:
-            missingCharacters.append(char)
 
 sharedCharacters = sorted(sharedCharacters)
 uniqueCharacters = sorted(uniqueCharacters)
