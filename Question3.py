37c123f6de98c2b4b854db2cae552d2521fa6dc6
hau_v <hau_vu94@hotmail.com>
2023-04-21 12:20:57 +0200

    completed question 3

---
+++ Question3.py
@@ -0,0 +1,12 @@
+
+def checkPalinedrome(input):
+    return input == input[::-1]
+
+
+
+userInput = input("Write in a String: ")
+
+if checkPalinedrome(userInput):
+    print(f"{userInput} is a palindrome")
+else:
+    print(f"{userInput} is not a palindrome")
